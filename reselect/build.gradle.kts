3c2c192c42910fbd26eecc9d71a072b630733d25
Digifox03 <digifox03@protonmail.com>
2021-07-19 01:07:19 +0200

    Fabric mod template

---
+++ reselect/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    id("fabric-loom")
+    kotlin("jvm")
+}
+
+val modVersion: String by project
+val mavenGroup: String by project
+val minecraftVersion: String by project
+val yarnMappings: String by project
+val loaderVersion: String by project
+val fabricVersion: String by project
+val fabricKotlinVersion: String by project
+val archiveName: String by project
+
+version = modVersion
+group = mavenGroup
+
+base {
+    archivesName.set(archiveName)
+}
+
+dependencies {
+    minecraft("com.mojang", "minecraft", minecraftVersion)
+    mappings("net.fabricmc", "yarn", yarnMappings, classifier="v2")
+    modImplementation("net.fabricmc", "fabric-loader", loaderVersion)
+    // modImplementation("net.fabricmc.fabric-api", "fabric-api", fabricVersion)
+    modImplementation("net.fabricmc", "fabric-language-kotlin", fabricKotlinVersion)
+}
+
+java {
+    withSourcesJar()
+    sourceCompatibility = JavaVersion.VERSION_16
+    targetCompatibility = JavaVersion.VERSION_16
+}
+
+tasks.withType<ProcessResources> {
+    inputs.property("version", modVersion)
+    filesMatching("fabric.mod.json") {
+        expand("version" to modVersion)
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.release.set(16)
+}
+
+tasks.withType<Jar> {
+    from("LICENSE") {
+        rename { "${it}_${this@Build_gradle.archiveName}" }
+    }
+}
+
